a763180a6d6be0d63fef175ec8fc66020fa17ade
Chrisdion Andrew <iniandrewrp@gmail.com>
2024-08-27 20:12:25 +0700

    update port and host value

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
-        port: '8000',
-        host: 'localhost',
+        port: process.env.NODE_ENV !== 'production' ? '8000' : '5000', // using port 8000 because port 5000 is used by airplay server in mac.
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
